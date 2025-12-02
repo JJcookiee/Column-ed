c2006104379882a98ff1af3d68259643b1d32d1b
Jacob Jones <jacobaljones@gmail.com>
2025-12-02 14:16:50 +0000

    making this simpler

--- db/schema.sql
+++ db/schema.sql
@@ -1,23 +1,43 @@
 create table media(
-    primary key media_id int not null,
+    media_id int not null primary key,
     title varchar(255) not null,
-    desc varchar(500) not null,
+    descr varchar(500) not null,
     media_type int not null,
     release_date date not null 
 );
 
 create table users(
-    primary key user_id int not null,
+    user_id int not null primary key,
     user_name varchar(50) not null,
     password_hash varchar(50) not null,
     email varchar(255) not null
 );
 
 create table reviews(
-    primary key review_id int not null,
+    review_id int not null primary key,
     title varchar(255) not null,
     post_date date not null,
-    desc varchar(500) not null,
-    foreign key media_id int not null,
-    foreign key user_id int not null
+    descr varchar(500) not null,
+    media_id int not null references media(media_id),
+    user_id int not null references users(user_id)
+);
+
+create table comment(
+    comment_id int not null primary key,
+    post_date date not null,
+    comment_text varchar(500) not null,
+    user_id int not null references users(user_id),
+    review_id int not null references reviews(review_id)
+);
+
+create table review_likes(
+    review_id int not null references reviews(review_id),
+    user_id int not null references users(user_id),
+    is_liked boolean not null
+);
+
+create table comment_likes(
+    comment_id int not null references comments(comment_id),
+    user_id int not null references users(user_id),
+    is_liked boolean not null
 );
