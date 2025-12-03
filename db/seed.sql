0c69d15e215add622828bfc31559dcf9f8da3ead
Jacob Jones <jacobaljones@gmail.com>
2025-12-03 10:54:43 +0000

    seed v1.1

--- db/seed.sql
+++ db/seed.sql
@@ -3,10 +3,10 @@ insert into media_types(media_type,media_name)  values(2,'shows');
 insert into media_types(media_type,media_name)  values(3,'films');
 insert into media_types(media_type,media_name)  values(4,'books');
 
-insert into users(user_name,password_hash,email) values('john doe','this is not hashed','john.doe@email.com');
+insert into users(user_id,user_name,password_hash,email) values(1,'john doe','this is not hashed','john.doe@email.com');
 
-insert into reviews(title,post_date,descr,media_id,user_id) values('My review','2025-12-02','terrible',1,1);
+insert into reviews(review_id,title,post_date,descr,media_id,user_id) values(1,'My review','2025-12-02','terrible',1,1);
 
-insert into media(title,decr,media_type,release_date) values('test book','does not exist',4,'2025-12-02');
+insert into media(media_id,title,decr,media_type,release_date) values(1,'test book','does not exist',4,'2025-12-02');
 
-insert into books(auhtor,publisher,pages,chapters,media_id) values('me','also me',1,1,1)
+insert into books(book_id,auhtor,publisher,pages,chapters,media_id) values(1,'me','also me',1,1,1);
